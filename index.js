bd98ff0deea81db043cabedd217193773abed5ea
Ahmed <sanniahmed908@gmail.com>
2022-06-21 21:38:38 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var n1 = prompt('Please enter the first number');
+var n2 = prompt ('please enter the second number')
+var calculation = prompt('please a select a function +,-,/,*')
+
+if (calculation =='+') {
+  var result = n1+n2;
+  alert(result);
+}
+else if (calculation == '-') {
+    var result = n1-n2;
+    alert(result);
+}
+else if (calculation == '/') {
+    var result = n1/n2;
+    alert(result); 
+}
+else if (calculation ==  '*') {
+    var result = n1*n2;
+    alert(result);
+}
+else alert ('Wrong input , Try again later!!')
+var n1 = prompt('Please enter the first number');
+var n2 = prompt ('please enter the second number')
+var calculation = prompt('please a select a function +,-,/,*')
+
